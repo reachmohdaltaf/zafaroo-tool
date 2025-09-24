01d263581af429913fced3db23745d6f42d414c2
reachmohdaltaf <reachmohdaltaf@gmail.com>
2025-09-25 02:15:34 +0530

    dsfsdf

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const newsRoutes = require("./routes/news");
+const path = require("path");
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +12,14 @@ app.use(express.json());
 
 // Routes
 app.use("/api/news", newsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
