1682066926b5d016f1a2b8073d2d7d48130c8f1b
reachmohdaltaf <reachmohdaltaf@gmail.com>
2025-09-25 02:23:23 +0530

    fdssdfsdf

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,13 @@ app.use(express.json());
 app.use("/api/news", newsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 }
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
